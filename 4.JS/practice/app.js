2eee7212a40b9cdcc9d4338aaa765bf957e4c952
onkarsathe007 <onkarsathe96k@gmail.com>
2024-12-11 23:46:36 +0530

    Intermidiate JS concepts

--- 4.JS/practice/app.js
+++
@@ -1,8 +0,0 @@
-let pen = 10;
-let notebook = 50;
-// console.log("pen :"+pen)
-// above method is not a good method to write sentence so we use below method
-console.log(`pen price : ${pen}`);
-console.log(`Notebook price : ${notebook}`)
-console.log(`total price :${pen+notebook}`)
-// `` is known as a back tick,${} is template literal
