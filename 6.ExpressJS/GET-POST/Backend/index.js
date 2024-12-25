c012404240c6284fb427071f9e19050afb9f35dd
Onkar Sathe <onkarsathe96k@gmail.com>
2024-12-25 21:28:32 +0530

    Handelling the get and post request

---
+++ 6.ExpressJS/GET-POST/Backend/index.js
@@ -0,0 +1,21 @@
+const { urlencoded } = require("body-parser");
+const express =  require("express");
+const app = express();
+let port = 8080;
+
+app.use(urlencoded({extended : true}));
+app.use(express.json())
+
+app.listen(port,(req,res)=>{
+    console.log("Listening for port :",port);
+});
+
+app.get("/register",(req,res)=>{
+    let{user,pass} = req.query;
+    res.send(`Welcome ${user} great password ${pass}`);
+});
+
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
