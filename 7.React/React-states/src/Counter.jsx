9c41dddcbb829f48d1d5420b922113ba38c17242
Onkar <onkarsathe96k@gmail.com>
2025-09-06 15:18:03 +0530

    chore(Counter): update file

--- 7.React/React-states/src/Counter.jsx
+++ 7.React/React-states/src/Counter.jsx
@@ -2,18 +2,30 @@ import { useState,useEffect} from "react";
 
 function Counter() {
   const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(100);
+
+
 
   function incCount() {
-    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1); 
   }
+
+  function decCount() {
+    setCounter2((prevCounter) => prevCounter - 1);
+  }
+
+
+
   useEffect(()=>{
     console.log("Hello This is Onkar");
-  })
+  },[counter])
 
   return (
     <div>
       <h1>Count: {counter}</h1>
-      <button type="button" onClick={incCount}>Click</button>
+      <h1>Count: {counter2}</h1>
+      <button type="button" onClick={incCount}>inc</button>
+      <button type="button" onClick={decCount}>dec</button>
     </div>
   );
 }
